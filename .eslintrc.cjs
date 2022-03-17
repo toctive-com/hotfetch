66985e2a35cfd956905ca3d1e3e667abefa69e8f
Sameh Ashraf <sameh.elalfi.mail@gmail.com>
2022-03-18 01:11:30 +0200

    feat: add jest support to esline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,11 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -13,6 +15,13 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'jest',
   ],
-  rules: {},
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
 };
